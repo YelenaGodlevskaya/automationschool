6760a0602cfa58945272e61ddddba9ef9924a12e
yelena.godlevskaya <elena_m@inbox.lv>
2018-02-13 12:47:16 +0200

    test

--- src/week1/FirstLesson.java
+++ src/week1/FirstLesson.java
@@ -2,6 +2,70 @@ package week1;
 
 public class FirstLesson {
     public static void main(String[] args) {
+//        String a = "Hello,";
+//        String b = "World!";
+//        String c1 = a + " Java " + b;
+//        String c2 = String.format("%s Java %s", a, b);
+//        System.out.println(c2);
+//        String c3 = String.join(" ", a, "Java", b);
+//        System.out.println(c3);
+
+
+//        int a = 200;
+//        int b = 50;
+//        int c = a + b;
+//
+//        System.out.println("c variable =" + c);
+//
+//        int d = c / 4 * 2;
+//        System.out.println("d variable =" + d);
+//
+//        int e = 1000 - c * 2;
+//        System.out.println("e variable =" + e);
+//
+//        int f = (1000 - c) * 2;
+//        System.out.println("f variable =" + f);
+
+//        double a = 200.0;
+//        double b = 50.0;
+//        double c = a + b;
+//
+//        double d = c / 4.0 * 2.0;
+//
+//        double e = 1000.0 - c * 2.0;
+//
+//        double f = (1000.0 - c) * 2.0;
+//
+//        System.out.println("f variable =" + f);
+
+//        boolean a = 10>3;
+//                boolean b = a && 20 < 4;
+//        System.out.println(b);
+//        boolean c = 10>3;
+//        boolean c = a & b;
+//        boolean a = 10>3;
+//        if (a){
+//            System.out.println("a is true");
+//        }
+//            else{
+//            System.out.println("a is false");
+
+//        for (int i = 1; i <=10; i++) {
+//            System.out.println("count is: );
+
+//         int i = 1;
+//         while (i <= 100) {
+//             if (i>10){break;}
+//             System.out.println(i);
+//             i++;
+
+/*        int i = 1;
+        while (i <= 100) {
+            if (i == 5){continue;}
+            System.out.println(i);
+            i++;
+        }
+        }*/
 
     }
 }
